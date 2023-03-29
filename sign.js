3357f3e1f77d794bc548611aca1762263d8523a6
CameronLenartowich <camtl66@gmail.com>
2023-03-29 13:36:03 -0400

    updated

--- sign.js
+++ sign.js
@@ -4,12 +4,12 @@ const SHA256 = require('crypto-js/sha256');
 const ec = new EC('secp256k1');
 
 // TODO: fill in your hex private key
-const privateKey = "";
+const privateKey = "c0f97e7884a918040d6a8381fb37dbd88c7dc5fc439805d33912b2040687c764";
 
 const key = ec.keyFromPrivate(privateKey);
 
 // TODO: change this message to whatever you would like to sign
-const message = "I am in the ChainShot Bootcamp";
+const message = "Cryptography will make your head spin";
 
 const msgHash = SHA256(message);
 
--- verify.js
+++ verify.js
@@ -5,20 +5,20 @@ const ec = new EC('secp256k1');
 
 // TODO: fill in the public key points
 const publicKey = {
-  x: "",
-  y: ""
+  x: "c83207b7a7a6a1ada3476de3d7b7b4dcdb41a23e38343a812e0ff4028224ad2c",
+  y: "324ff8f2b763dffacecbbb8cc31b2318c244069bd5fa21fa8d20849fccc2bc76"
 }
 
 const key = ec.keyFromPublic(publicKey, 'hex');
 
 // TODO: change this message to whatever was signed
-const msg = "I am in the ChainShot Bootcamp";
+const msg = "I'm still shocked Team B won lol";
 const msgHash = SHA256(msg).toString();
 
 // TODO: fill in the signature components
 const signature = {
-  r: "",
-  s: ""
+  r: "ddf83e23ffc4a8a54d18306773dbd8e2fbd9a18fcd2dcf90f04f1e5f5991b824",
+  s: "5a6f88a4d93ac9fd0ccc9dc8987daf6de2b042f2cd972cf4688d331c2a6aa206"
 };
 
 console.log(key.verify(msgHash, signature));
